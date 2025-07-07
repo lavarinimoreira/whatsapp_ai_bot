e8a49124eac40f8fa5ab7d6e493859a17549eb40
lavarinimoreira <lavarinimoreira@gmail.com>
2025-07-07 17:11:40 -0300

    feat: add fastapi and uvicorn

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+
+
+app = FastAPI()
+
+
+@app.post('/webhook')
+async def webhook(request: Request):
+    data = await request.json()
+    print(data)
+    return {'status': 'ok'}
